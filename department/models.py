d2fd57eb31debdccae8cdde0f8c11069648cac28
root <mail@tolgahanuzun.com>
2015-09-10 23:23:06 +0300

    Models update

--- department/models.py
+++ department/models.py
@@ -3,10 +3,10 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class Department(models.Model):
-    departmenStasion = models.CharField(max_length=50,verbose_name="Departman Name")
-    departmenMembers = models.ManyToManyField(User,blank=True ,verbose_name="Departmen İn Members")
+    department_Name = models.CharField(max_length=50,verbose_name="Departman Name")
+    department_Members = models.ManyToManyField(User,blank=True ,verbose_name="Departmen İn Members")
 
     def __unicode__(self):
-        return "%s" % (self.departmenStasion)
+        return "%s" % (self.department_Name)
 
 
