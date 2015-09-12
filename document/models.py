eb74fd8d25aa2283a3cac1093fc73856d46e158d
root <mail@tolgahanuzun.com>
2015-09-12 22:18:28 +0300

    Document add and

--- document/models.py
+++ document/models.py
@@ -2,12 +2,13 @@
 from django.db import models
 from django.contrib.auth.models import User
 import datetime
+from django.utils import timezone
 
 # Create your models here.
 class Document(models.Model):
     name = models.CharField(max_length=50,verbose_name="Document Name")
     description = models.TextField(max_length=1000, verbose_name=u"Document description")
-    createtime = models.DateTimeField(verbose_name="Document Add Time")
+    createtime = models.DateTimeField(auto_now_add=True,verbose_name="Document Add Time")
     doc_file = models.FileField(upload_to="documentfiles/" ,verbose_name="Document Update")
     user = models.ForeignKey(User)
     send_Department = models.ManyToManyField("department.Department", verbose_name="Send To Department")
