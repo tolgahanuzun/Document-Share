80152985a153b56480b1b25a5b8360ac8e9a2a6d
root <mail@tolgahanuzun.com>
2015-09-13 19:33:20 +0300

    Add form new

--- document/forms.py
+++ document/forms.py
@@ -1,6 +1,6 @@
 #-*- coding: utf-8 -*-
 
-from django.db import forms
+from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate
 from django.core.exceptions import ValidationError
@@ -12,11 +12,31 @@ from django.core.files.base import ContentFile
 from django.db.models import Q
 from django.http import *
 
-
-from .models import *
-from profiles.models import * 
+from department.models import Department
+from document.models import Document
+from profiles.models import Profile 
 
 class AddDocument(forms.ModelForm):
-	name = forms.CharField(label=u"İsim", max_length=50, required=True)
+    name = forms.CharField(label=u"İsim", max_length=50, required=True)
     description = forms.CharField(label=u"Acıklama", max_length=1000, 
                                 required=True, widget=forms.Textarea)
+    send_Department = forms.ModelChoiceField(label=u"departmen Name",
+        queryset=Department.objects.all(), required=True)
+    class Meta:
+        model = Document
+        fields = ("name", "description","send_Department", "user")
+
+    
+    def save(self, commit=True):
+
+        document = Document()
+        document.name = self.data.get("name")
+        document.description = self.data.get("description")
+
+        send_Department = Department.objects.get(department_Name=
+            self.data.get("department_Name"))
+        document.send_Department = send_Department
+        document.user = self.user
+        document.save()
+ 
+        return document    
