2ef9edcc7275641561595b5ff18df7615279a91a
root <mail@tolgahanuzun.com>
2015-09-13 19:34:03 +0300

    Document add page, def update

--- document/views.py
+++ document/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
+#-*- coding: utf-8 -*-
 
-# Create your views here.
+from django.shortcuts import render, render_to_response, get_object_or_404
+from django.http import *
+from django.template import Template, Context, RequestContext, loader
+from django.contrib.auth.models import User
+from django.contrib.auth import login as auth_login, logout as auth_logout, authenticate
+from django.contrib.auth.decorators import login_required
+
+from document.models import Document
+from document.forms import AddDocument
+
+def addDocumentViews(request):
+    
+    if request.user.is_authenticated():
+        if request.method == "POST":
+            form_addDocument = AddDocument(request.POST, user=request.user)
+            if form_addDocument.is_valid():
+                form_addDocument = form_addDocument.save()
+                meal.save() 
+                return HttpResponseRedirect("/")
+        else:
+            form_addDocument = AddDocument()
+            return render(request, "add.html", {'form_addDocument':form_addDocument,})  
+    else:
+        return HttpResponseRedirect("/")
+
