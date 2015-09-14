7b18e0547262c296937a5d9d42cad0d90e494371
root <mail@tolgahanuzun.com>
2015-09-14 22:26:17 +0300

    Forms html request data update

--- document/views.py
+++ document/views.py
@@ -6,22 +6,22 @@ from django.template import Template, Context, RequestContext, loader
 from django.contrib.auth.models import User
 from django.contrib.auth import login as auth_login, logout as auth_logout, authenticate
 from django.contrib.auth.decorators import login_required
-
+from department.models import Department
 from document.models import Document
 from document.forms import AddDocument
 
 def addDocumentViews(request):
-    
+    Depart = Department.objects.all()
     if request.user.is_authenticated():
         if request.method == "POST":
             form_addDocument = AddDocument(request.POST, user=request.user)
             if form_addDocument.is_valid():
                 form_addDocument = form_addDocument.save()
-                meal.save() 
+                AddDocument.save() 
                 return HttpResponseRedirect("/")
         else:
             form_addDocument = AddDocument()
-            return render(request, "add.html", {'form_addDocument':form_addDocument,})  
+            return render(request, "add.html", {'form_addDocument':form_addDocument,"Depart":Depart})  
     else:
         return HttpResponseRedirect("/")
 
