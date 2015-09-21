5ede1117cb13734b2976026838411256a6e5c83e
root <mail@tolgahanuzun.com>
2015-09-21 20:55:08 +0300

    Last İndex page js clean data and timeline js update

--- static/js/index.js
+++ static/js/index.js
@@ -1,10 +1,4 @@
 $(function () {
-    /* START OF DEMO JS - NOT NEEDED */
-    if (window.location == window.parent.location) {
-        $('#fullscreen').html('<span class="glyphicon glyphicon-resize-small"></span>');
-        $('#fullscreen').attr('href', 'http://bootsnipp.com/mouse0270/snippets/PbDb5');
-        $('#fullscreen').attr('title', 'Back To Bootsnipp');
-    }    
     $('#fullscreen').on('click', function(event) {
         event.preventDefault();
         window.parent.location =  $('#fullscreen').attr('href');
@@ -16,4 +10,13 @@ $(function () {
 		event.preventDefault();
 		$(this).closest('.navbar-minimal').toggleClass('open');
 	})
+});
+
+
+/*Timeline Aktifleştirme*/
+$(function () {
+    $('.timeline-panel').click(function() {
+        $('.timeline-body', this).toggle();
+    });
+
 });
